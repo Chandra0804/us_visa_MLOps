fac4f74fe94811d3941cd1e20fd0164a5ab58205
Chandra0804 <chandrasaiteja0804@gmail.com>
2024-10-02 16:49:56 +0530

    Data Validation

--- us_visa/components/data_validation.py
+++ us_visa/components/data_validation.py
@@ -1,168 +1,138 @@
+import json
 import sys
+
+import pandas as pd
+from evidently.model_profile import Profile
+from evidently.model_profile.sections import DataDriftProfileSection
+
+from pandas import DataFrame
+
 from us_visa.exception import USvisaException
 from us_visa.logger import logging
-from us_visa.components.data_ingestion import DataIngestion
-from us_visa.components.data_validation import DataValidation
-from us_visa.components.data_transformation import DataTransformation
-from us_visa.components.model_trainer import ModelTrainer
-from us_visa.components.model_evaluation import ModelEvaluation
-from us_visa.components.model_pusher import ModelPusher
+from us_visa.utils.main_utils import read_yaml_file, write_yaml_file
+from us_visa.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from us_visa.entity.config_entity import DataValidationConfig
+from us_visa.constants import SCHEMA_FILE_PATH
 
 
-from us_visa.entity.config_entity import (DataIngestionConfig,
-                                         DataValidationConfig,
-                                         DataTransformationConfig,
-                                         ModelTrainerConfig,
-                                         ModelEvaluationConfig,
-                                         ModelPusherConfig)
-
-from us_visa.entity.artifact_entity import (DataIngestionArtifact,
-                                            DataValidationArtifact,
-                                            DataTransformationArtifact,
-                                            ModelTrainerArtifact,
-                                            ModelEvaluationArtifact,
-                                            ModelPusherArtifact)
-
-
-class TrainPipeline:
-    def __init__(self):
-        self.data_ingestion_config = DataIngestionConfig()
-        self.data_validation_config = DataValidationConfig()
-        self.data_transformation_config = DataTransformationConfig()
-        self.model_trainer_config = ModelTrainerConfig()
-        self.model_evaluation_config = ModelEvaluationConfig()
-        self.model_pusher_config = ModelPusherConfig()
-
-
-    
-    def start_data_ingestion(self) -> DataIngestionArtifact:
-        """
-        This method of TrainPipeline class is responsible for starting data ingestion component
-        """
+class DataValidation:
+    def __init__(self, data_ingestion_artifact: DataIngestionArtifact, data_validation_config: DataValidationConfig):
         try:
-            logging.info("Entered the start_data_ingestion method of TrainPipeline class")
-            logging.info("Getting the data from mongodb")
-            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
-            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-            logging.info("Got the train_set and test_set from mongodb")
-            logging.info(
-                "Exited the start_data_ingestion method of TrainPipeline class"
-            )
-            return data_ingestion_artifact
+            self.data_ingestion_artifact = data_ingestion_artifact
+            self.data_validation_config = data_validation_config
+            self._schema_config =read_yaml_file(file_path=SCHEMA_FILE_PATH)
+        except Exception as e:
+            raise USvisaException(e,sys)
+
+    def validate_number_of_columns(self, dataframe: DataFrame) -> bool:
+
+        try:
+            status = len(dataframe.columns) == len(self._schema_config["columns"])
+            logging.info(f"Is required column present: [{status}]")
+            return status
+        except Exception as e:
+            raise USvisaException(e, sys)
+
+    def is_column_exist(self, df: DataFrame) -> bool:
+
+        try:
+            dataframe_columns = df.columns
+            missing_numerical_columns = []
+            missing_categorical_columns = []
+            for column in self._schema_config["numerical_columns"]:
+                if column not in dataframe_columns:
+                    missing_numerical_columns.append(column)
+
+            if len(missing_numerical_columns)>0:
+                logging.info(f"Missing numerical column: {missing_numerical_columns}")
+
+
+            for column in self._schema_config["categorical_columns"]:
+                if column not in dataframe_columns:
+                    missing_categorical_columns.append(column)
+
+            if len(missing_categorical_columns)>0:
+                logging.info(f"Missing categorical column: {missing_categorical_columns}")
+
+            return False if len(missing_categorical_columns)>0 or len(missing_numerical_columns)>0 else True
         except Exception as e:
             raise USvisaException(e, sys) from e
-        
 
-    
-    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
-        """
-        This method of TrainPipeline class is responsible for starting data validation component
-        """
-        logging.info("Entered the start_data_validation method of TrainPipeline class")
+    @staticmethod
+    def read_data(file_path) -> DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise USvisaException(e, sys)
+
+    def detect_dataset_drift(self, reference_df: DataFrame, current_df: DataFrame, ) -> bool:
 
         try:
-            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
-                                             data_validation_config=self.data_validation_config
-                                             )
+            data_drift_profile = Profile(sections=[DataDriftProfileSection()])
 
-            data_validation_artifact = data_validation.initiate_data_validation()
+            data_drift_profile.calculate(reference_df, current_df)
 
-            logging.info("Performed the data validation operation")
+            report = data_drift_profile.json()
+            json_report = json.loads(report)
 
-            logging.info(
-                "Exited the start_data_validation method of TrainPipeline class"
+            write_yaml_file(file_path=self.data_validation_config.drift_report_file_path, content=json_report)
+
+            n_features = json_report["data_drift"]["data"]["metrics"]["n_features"]
+            n_drifted_features = json_report["data_drift"]["data"]["metrics"]["n_drifted_features"]
+
+            logging.info(f"{n_drifted_features}/{n_features} drift detected.")
+            drift_status = json_report["data_drift"]["data"]["metrics"]["dataset_drift"]
+            return drift_status
+        except Exception as e:
+            raise USvisaException(e, sys) from e
+
+    def initiate_data_validation(self) -> DataValidationArtifact:
+
+        try:
+            validation_error_msg = ""
+            logging.info("Starting data validation")
+            train_df, test_df = (DataValidation.read_data(file_path=self.data_ingestion_artifact.trained_file_path),
+                                 DataValidation.read_data(file_path=self.data_ingestion_artifact.test_file_path))
+
+            status = self.validate_number_of_columns(dataframe=train_df)
+            logging.info(f"All required columns present in training dataframe: {status}")
+            if not status:
+                validation_error_msg += f"Columns are missing in training dataframe."
+            status = self.validate_number_of_columns(dataframe=test_df)
+
+            logging.info(f"All required columns present in testing dataframe: {status}")
+            if not status:
+                validation_error_msg += f"Columns are missing in test dataframe."
+
+            status = self.is_column_exist(df=train_df)
+
+            if not status:
+                validation_error_msg += f"Columns are missing in training dataframe."
+            status = self.is_column_exist(df=test_df)
+
+            if not status:
+                validation_error_msg += f"columns are missing in test dataframe."
+
+            validation_status = len(validation_error_msg) == 0
+
+            if validation_status:
+                drift_status = self.detect_dataset_drift(train_df, test_df)
+                if drift_status:
+                    logging.info(f"Drift detected.")
+                    validation_error_msg = "Drift detected"
+                else:
+                    validation_error_msg = "Drift not detected"
+            else:
+                logging.info(f"Validation_error: {validation_error_msg}")
+                
+
+            data_validation_artifact = DataValidationArtifact(
+                validation_status=validation_status,
+                message=validation_error_msg,
+                drift_report_file_path=self.data_validation_config.drift_report_file_path
             )
 
+            logging.info(f"Data validation artifact: {data_validation_artifact}")
             return data_validation_artifact
-
         except Exception as e:
             raise USvisaException(e, sys) from e
-        
-
-
-    def start_data_transformation(self, data_ingestion_artifact: DataIngestionArtifact, data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
-        """
-        This method of TrainPipeline class is responsible for starting data transformation component
-        """
-        try:
-            data_transformation = DataTransformation(data_ingestion_artifact=data_ingestion_artifact,
-                                                     data_transformation_config=self.data_transformation_config,
-                                                     data_validation_artifact=data_validation_artifact)
-            data_transformation_artifact = data_transformation.initiate_data_transformation()
-            return data_transformation_artifact
-        except Exception as e:
-            raise USvisaException(e, sys)
-        
-
-    
-    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
-        """
-        This method of TrainPipeline class is responsible for starting model training
-        """
-        try:
-            model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
-                                         model_trainer_config=self.model_trainer_config
-                                         )
-            model_trainer_artifact = model_trainer.initiate_model_trainer()
-            return model_trainer_artifact
-
-        except Exception as e:
-            raise USvisaException(e, sys)
-        
-    
-
-    def start_model_evaluation(self, data_ingestion_artifact: DataIngestionArtifact,
-                               model_trainer_artifact: ModelTrainerArtifact) -> ModelEvaluationArtifact:
-        """
-        This method of TrainPipeline class is responsible for starting modle evaluation
-        """
-        try:
-            model_evaluation = ModelEvaluation(model_eval_config=self.model_evaluation_config,
-                                               data_ingestion_artifact=data_ingestion_artifact,
-                                               model_trainer_artifact=model_trainer_artifact)
-            model_evaluation_artifact = model_evaluation.initiate_model_evaluation()
-            return model_evaluation_artifact
-        except Exception as e:
-            raise USvisaException(e, sys)
-        
-
-    
-    def start_model_pusher(self, model_evaluation_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
-        """
-        This method of TrainPipeline class is responsible for starting model pushing
-        """
-        try:
-            model_pusher = ModelPusher(model_evaluation_artifact=model_evaluation_artifact,
-                                       model_pusher_config=self.model_pusher_config
-                                       )
-            model_pusher_artifact = model_pusher.initiate_model_pusher()
-            return model_pusher_artifact
-        except Exception as e:
-            raise USvisaException(e, sys)
-
-        
-        
-        
-
-    
-    def run_pipeline(self, ) -> None:
-        """
-        This method of TrainPipeline class is responsible for running complete pipeline
-        """
-        try:
-            data_ingestion_artifact = self.start_data_ingestion()
-            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
-            data_transformation_artifact = self.start_data_transformation(
-                data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
-            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
-            model_evaluation_artifact = self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact,
-                                                                    model_trainer_artifact=model_trainer_artifact)
-            
-            if not model_evaluation_artifact.is_model_accepted:
-                logging.info(f"Model not accepted.")
-                return None
-            model_pusher_artifact = self.start_model_pusher(model_evaluation_artifact=model_evaluation_artifact)
-
-        except Exception as e:
-            raise USvisaException(e, sys)
-        
